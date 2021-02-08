67177643a9fbc51d760218083a512bc8c2894717
Antti Palo <anttipalo8@gmail.com>
2021-02-08 15:31:19 +0200

    Added a placeholder scene with basic player movement

---
+++ BTL/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D playerRb;
+
+    private bool playerOnGround;
+    public Transform groundCheck;
+    public LayerMask groundLayerMask;
+
+    private float groundCheckRadius = .2f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movePlayer();
+        CheckGround();
+        Jump();
+    }
+
+    //Player movement
+    void movePlayer()
+    {
+        playerRb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), playerRb.velocity.y);
+    }
+
+    //Check if player is on the ground
+    void CheckGround()
+    {
+        playerOnGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayerMask);
+    }
+
+    //Jumping
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && playerOnGround)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+    }
+}
