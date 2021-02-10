38f102008c806850542ba21ab268bbf07467b4e1
Aleksi Putkonen <alluputkonen@gmail.com>
2021-02-10 17:07:29 +0200

    Settings menu created
    Created settings menu
    Volume slider, does not have functions yet

--- BTL/Assets/Scripts/MainMenu2.cs
+++ BTL/Assets/Scripts/MainMenu2.cs
@@ -5,14 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu2 : MonoBehaviour
 {
+    //Main Menu play button function
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //Main Menu Settings button function
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    //Main Menu quit button function
     public void QuitGame()
     {
         Application.Quit();
     }
 
+    // Settings menu Back button functions
+    public void BackBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
 }
---
+++ BTL/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    /*
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    } 
+
+    public void BackBtn()
+    {
+        SceneManager.BackBtn(SceneManager.GetActiveScene().buildIndex - 2);
+    } */
+}
