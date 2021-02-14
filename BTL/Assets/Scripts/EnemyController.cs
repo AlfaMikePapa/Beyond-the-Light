9f7693c8fcd39f94f32cfc73adc0ebe79019eb7b
Antti Palo <anttipalo8@gmail.com>
2021-02-14 17:37:33 +0200

    Added enemies that move between points and changed scene name

---
+++ BTL/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform leftPoint, rightPoint;
+    private Rigidbody2D enemyRB;
+    private bool movingRight;
+    
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+
+        //unparent the checkpoints so that they don't move with the enemy
+        leftPoint.parent = null;
+        rightPoint.parent = null;
+
+        movingRight = true;
+    }
+
+    void Update()
+    {
+        MoveBetweenPoints();
+    }
+
+    public void MoveBetweenPoints()
+    {
+        if (movingRight)
+        {
+            enemyRB.velocity = new Vector2(moveSpeed, enemyRB.velocity.y);
+
+            if (transform.position.x > rightPoint.position.x)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            enemyRB.velocity = new Vector2(-moveSpeed, enemyRB.velocity.y);
+
+            if (transform.position.x < leftPoint.position.x)
+            {
+                movingRight = true;
+            }
+        }
+    }
+}
+
+
