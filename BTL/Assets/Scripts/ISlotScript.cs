ab0f66e76711f8ead201f485d7ddb26d48cba544
Aleksi Putkonen <alluputkonen@gmail.com>
2021-03-01 21:34:53 +0200

    Added code for inventory
    Will need more work once pickable items are created

---
+++ BTL/Assets/Scripts/ISlotScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ISlotScript : MonoBehaviour
+{ 
+    /*
+     Will be used later once pickable items are created!
+     
+    public Image icon;
+
+    Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+
+        icon.sprite = item.icon;
+        icon.enabled = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+    */
+}
---
+++ BTL/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{ /*
+   Will be used later once pickable items are created!
+   
+    InventoryUI inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = InventoryUI.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+    }
+    */
+}
