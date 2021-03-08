b517708d711c507538c02939c352c121b149f188
Antti Palo <anttipalo8@gmail.com>
2021-03-08 14:58:19 +0200

    Level end (W.I.P)

---
+++ BTL/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public string nextLevel;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void EndLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
