7e72163d1fd82d01e44a9307d700ae7527f8ee98
DESKTOP-4HFQJ6D\Piitu <mikkopiitu@gmail.com>
2021-03-08 21:12:00 +0200

    Added global light dimming when underground

---
+++ BTL/Assets/Scripts/GlobalLightControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Experimental.Rendering.LWRP;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLightControl : MonoBehaviour
+{
+    UnityEngine.Experimental.Rendering.Universal.Light2D myLight;
+    
+    public GameObject Player;
+
+    public float speed; //Speed of the fade
+    private float direction = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    private void Update()
+    {           
+        //Reduce light to 0.2f when player is below -8
+        if (myLight.intensity >= 0.2f && Player.transform.position.y < -8)
+        {
+            myLight.intensity += Time.deltaTime * speed * direction;
+            direction = -1f;
+        }
+
+        //Increase light to 1.0f when player is above -8
+        if (myLight.intensity <= 1.0f && Player.transform.position.y > -8)
+        {
+            myLight.intensity += Time.deltaTime * speed * direction;
+            direction = 1f;
+        }
+    }
+}
