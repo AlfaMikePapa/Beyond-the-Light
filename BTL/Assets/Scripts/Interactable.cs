8845d9c91116fba1668b7f516b78d915df3d71d2
Aleksi Putkonen <64532516+AlfaMikePapa@users.noreply.github.com>
2021-03-14 12:50:01 +0200

    Code for item picking and inventory

---
+++ BTL/Assets/Scripts/Interactable.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        //This method is ment to be overwritten
+        Debug.Log("Interacting with" + transform.name);
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        /*
+        Gizmos.color = Color.Yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+        */
+    }
+
+}
+
