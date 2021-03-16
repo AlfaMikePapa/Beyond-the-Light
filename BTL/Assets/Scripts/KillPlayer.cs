b625f5fddf498cc9dffed6a45121a9cbd58a27e6
Antti Palo <anttipalo8@gmail.com>
2021-03-17 00:28:03 +0200

    Sensuroitu kommentteja koodissa :D

--- BTL/Assets/Scripts/KillPlayer.cs
+++ BTL/Assets/Scripts/KillPlayer.cs
@@ -6,12 +6,9 @@ public class KillPlayer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("sisällä ollaan");
-
         if (other.tag == "Player")
         {
             PlayerHealthController.instance.DealDamage(100);
-            Debug.Log("Kuole paska");
         }
     }
 }
