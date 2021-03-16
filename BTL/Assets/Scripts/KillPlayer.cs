39a43e5c0e154e8757afa8fcfa69086d1e630601
Antti Palo <anttipalo8@gmail.com>
2021-03-16 19:40:38 +0200

    Added killboxes to the spikes

---
+++ BTL/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("sisällä ollaan");
+
+        if (other.tag == "Player")
+        {
+            PlayerHealthController.instance.DealDamage(100);
+            Debug.Log("Kuole paska");
+        }
+    }
+}
