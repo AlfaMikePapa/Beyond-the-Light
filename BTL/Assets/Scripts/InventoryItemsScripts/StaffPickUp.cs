2c4d8cfd3b81403b4015a409cd098975c0a68cfb
Aleksi <alluputkonen@gmail.com>
2021-03-16 23:40:21 +0200

    Working pickup script added

---
+++ BTL/Assets/Scripts/InventoryItemsScripts/StaffPickUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaffPickUp : MonoBehaviour
+{
+
+    [SerializeField]
+ // private Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    /*
+    // Use this for initialization
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+    */ 
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+        //  pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+            Debug.Log("OTA SAUVA SAaTANA ");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+        //  pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        Destroy(gameObject);
+    }
+
+}
